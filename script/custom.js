5cc4fb7c3775d28f25b3e65299fc38790ede69bc
richardtraindl <richard.traindl@chello.at>
2013-06-21 15:44:07 +0200

    gallery und javascript

--- script/custom.js
+++ script/custom.js
@@ -1,23 +1,27 @@
 
 
-	function debug( msg ){
-	   if( window.console )
-		 window.console.log( msg );
+	function selMenueItemActiv(){
+		var BodyId = 	$("body").attr("id");
+		$('nav#mainnav a[href="' + BodyId + '.html"]').addClass('mainnav-active');
 	}
 
-	function showsel(anker){
-	   debug( "enter showsel: " + anker );
+	function showSection(){
+		$('div#subnav a').attr('href', '#');
+		$("section").removeClass("aktiv");
+		if(window.location.hash == ""){
+			$("section:first").addClass("aktiv");
+		}
+		else{
+			$("section" + window.location.hash).addClass("aktiv");
+			$("div#subnav a").removeClass("nav-active");  
+			$('div#subnav a#nav_' + window.location.hash.substr(1)).addClass("nav-active");
+		}
+	}
 
+	function showSelSection(anker){
 		$("section").removeClass("aktiv");  
 		$('section#' + anker).addClass("aktiv");
 	 
 		$("div#subnav a").removeClass("nav-active");  
 		$('div#subnav a#nav_' + anker).addClass("nav-active");	
-
-	  debug( "leave showsel: " + anker );	
-	}
-
-	function selactiv(){
-		var BodyId = 	$("body").attr("id");
-		$('nav#mainnav a[href="' + BodyId + '.html"]').addClass('mainnav-active');
 	}
