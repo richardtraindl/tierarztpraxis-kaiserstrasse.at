aca45243b753949f39430b051a73f9dbd0a6cc4f
richardtraindl <richardtraindl@gmail.com>
2014-05-24 11:04:27 +0200

    new layout nr. 3

---
+++ _includes/scripts/custom.js
@@ -0,0 +1,27 @@
+
+
+	function selMenueItemActiv(){
+		var BodyId = 	$("body").attr("id");
+		$('nav#mainnav a[href="' + BodyId + '.html"]').addClass('mainnav-active');
+	}
+
+	function showSection(){
+		$('div#subnav a').attr('href', '#');
+		$("section").removeClass("aktiv");
+		if(window.location.hash == ""){
+			$("section:first").addClass("aktiv");
+		}
+		else{
+			$("section" + window.location.hash).addClass("aktiv");
+			$("div#subnav a").removeClass("nav-active");  
+			$('div#subnav a#nav_' + window.location.hash.substr(1)).addClass("nav-active");
+		}
+	}
+
+	function showSelSection(anker){
+		$("section").removeClass("aktiv");  
+		$('section#' + anker).addClass("aktiv");
+	 
+		$("div#subnav a").removeClass("nav-active");  
+		$('div#subnav a#nav_' + anker).addClass("nav-active");
+	}
--- scripts/custom.js
+++ scripts/custom.js
@@ -2,26 +2,27 @@
 
 	function selMenueItemActiv(){
 		var BodyId = 	$("body").attr("id");
-		$('nav#mainnav a[href="' + BodyId + '.html"]').addClass('mainnav-active');
+		$('nav#mainnav a[href="' + BodyId + '.html"]').addClass('nav-active');
 	}
 
 	function showSection(){
-		$('div#subnav a').attr('href', '#');
+		// $('nav a').attr('href', '#');
 		$("section").removeClass("aktiv");
-		if(window.location.hash == ""){
-			$("section:first").addClass("aktiv");
-		}
-		else{
-			$("section" + window.location.hash).addClass("aktiv");
-			$("div#subnav a").removeClass("subnav-active");  
-			$('div#subnav a#nav_' + window.location.hash.substr(1)).addClass("subnav-active");
-		}
+		$("section:first").addClass("aktiv");
+		// if(window.location.hash == ""){
+		// 	$("section:first").addClass("aktiv");
+		// }
+		// else{
+			// $("section" + window.location.hash).addClass("aktiv");
+			// $("nav a").removeClass("nav-active");  
+			//  $('nav a#nav_' + window.location.hash.substr(1)).addClass("nav-active");
+		// }
 	}
 
 	function showSelSection(anker){
 		$("section").removeClass("aktiv");  
 		$('section#' + anker).addClass("aktiv");
 	 
-		$("div#subnav a").removeClass("subnav-active");  
-		$('div#subnav a#nav_' + anker).addClass("subnav-active");	
+		$("nav#mainnav a").removeClass("nav-active");  
+		$('nav#mainnav a#nav_' + anker).addClass("nav-active");	
 	}
