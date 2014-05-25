ca11a268f0e003dc980e4089068687aefce619a5
richardtraindl <richardtraindl@gmail.com>
2014-05-25 09:41:36 +0200

    javascript improved

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,35 +1,62 @@
 
 
 	function selMenueItemActiv(){
-		var BodyId = 	$("body").attr("id");
-		mainnav = $('nav#mainnav a[href="' + BodyId + '.html"]');
-		mainnav.addClass('nav-active');
+		var bodyid = 	$("body").attr("id");
+		
+		var divitem = $("#nav_" + bodyid);
 
-		var subnav = mainnav.nextAll('div.submenu-item');
-		if( subnav != null ){
-			subnav.addClass('aktiv');
-			subnav.find('a').first().addClass('nav-active');
-		}		
+		var subitems = divitem.children('div.submenu-item');
+		subitems.addClass('aktiv');
+				
+		if(window.location.hash == ""){
+			if( subitems.length == 0 ){
+				divitem.find('a').first().addClass('nav-aktiv');
+			}
+			else{
+				subitems.find('a').first().addClass('nav-aktiv');
+			}
+		}
+		else{
+			var stringarr = window.location.hash.split('#');
+			
+			$('nav a#nav_' + stringarr[1]).addClass("nav-aktiv");
+		}			
 	}
 
 	function showSection(){
-		// $('nav a').attr('href', '#');
 		$("section").removeClass("aktiv");
-		$("section:first").addClass("aktiv");
-		// if(window.location.hash == ""){
-		// 	$("section:first").addClass("aktiv");
-		// }
-		// else{
-			// $("section" + window.location.hash).addClass("aktiv");
-			// $("nav a").removeClass("nav-active");  
-			//  $('nav a#nav_' + window.location.hash.substr(1)).addClass("nav-active");
-		// }
+		
+		if(window.location.hash == ""){
+			$("section:first").addClass("aktiv");
+		}
+		else{
+			var stringarr = window.location.hash.split('#');
+			$("section#" + stringarr[1]).addClass("aktiv");
+		}
 	}
 
 	function showSelSection(anker){
+		var stringarr = anker.split('#');
+
 		$("section").removeClass("aktiv");  
-		$('section#' + anker).addClass("aktiv");
+		$('section#' + stringarr[1]).addClass("aktiv");
+		
+		$("nav#mainnav a").removeClass("nav-aktiv"); 
+		$('nav a#nav_' + stringarr[1]).addClass("nav-aktiv");
 	 
-		$("nav#mainnav a").removeClass("nav-active");  
-		$('nav#mainnav a#nav_' + anker).addClass("nav-active");	
+		/* 
+
+		var item = $('a#nav_' + anker).parents('div.menu-item').first();
+		var subitems = item.children('div.submenu-item');
+		
+		if(window.location.hash == ""){
+			alert("+++");
+			subitems.addClass('aktiv');
+			$('a#nav_' + anker).addClass("nav-aktiv");
+		}
+		else{
+			alert(":::" + window.location.hash.substr(1));
+			subitems.addClass('aktiv');
+			$('a#nav_' + window.location.hash.substr(1)).addClass("nav-aktiv");
+		} */
 	}
