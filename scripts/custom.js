22984bcd9ff476dc8ae8b3bd9b77af36adeeb131
richardtraindl <richardtraindl@gmail.com>
2016-01-09 19:17:09 +0100

    big rebuild

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,69 +1,14 @@
 
   function selMenueItemActiv(){
-    var bodyid = 	$("body").attr("id");
-    
-    var divitem = $("#nav_" + bodyid);
+    var bodyid = $("body").attr("id");
+    var stringarr = bodyid.split('-');
+    var divitem = $("#nav_" + stringarr[0]);
 
     var subitems = divitem.children('div.submenu-item');
     
-    if(window.screen.width < 640){
-      if(window.location.hash == ""){
-        divitem.find('a').first().addClass('nav-aktiv');
-      }
-      else{
-        var stringarr = window.location.hash.split('#');
-        
-        $('nav a#nav_' + stringarr[1]).addClass("nav-aktiv");
-      }
-
-      return;
-    }
-
-    subitems.addClass('aktiv');  
-
-    if(window.location.hash == ""){
-      if( subitems.length == 0 ){
-        divitem.find('a').first().addClass('nav-aktiv');
-      }
-      else{
-        subitems.find('a').first().addClass('nav-aktiv');
-      }
-    }
-    else{
-      var stringarr = window.location.hash.split('#');
-      
-      $('nav a#nav_' + stringarr[1]).addClass("nav-aktiv");
-    }
-  }
-
-  function showSection(){
-    if(window.screen.width < 640){
-      return;
-    }
-    
-    var varsection;
-
-    $("section").removeClass("aktiv");
-    
-    if(window.location.hash == ""){
-      varsection = $("section:first");
-      varsection.addClass("aktiv");
-    }
-    else{
-      var stringarr = window.location.hash.split('#');
-      varsection = $("section#" + stringarr[1]);
-      varsection.addClass("aktiv");
-    }
-  }
-
-  function showSelSection(anker){
-    var varsection;
-    var stringarr = anker.split('#');
-
-    $("section").removeClass("aktiv"); 
-    varsection = $('section#' + stringarr[1]);
-    $(varsection).addClass("aktiv");
-
-    $("nav#mainnav a").removeClass("nav-aktiv"); 
     $('nav a#nav_' + stringarr[1]).addClass("nav-aktiv");
+    
+    if(window.screen.width >= 640){
+        subitems.addClass('aktiv');
+    }
   }
