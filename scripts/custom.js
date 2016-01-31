f23ba6e64073235b2a13802febba11ecec0b16e8
richardtraindl <richardtraindl@gmail.com>
2016-01-31 17:43:03 +0100

    medium layout

--- scripts/custom.js
+++ scripts/custom.js
@@ -15,12 +15,12 @@
 
     function startSlider(){
       $('figure.image').nivoSlider({
-          effect: 'fade',                 // Specify sets like: 'fold,fade,sliceDown'
+          effect: 'fade',            // Specify sets like: 'fold,fade,sliceDown'
           slices: 15,                     // For slice animations
-          boxCols: 8,                     // For box animations
-          boxRows: 4,                     // For box animations
-          animSpeed: 500,                 // Slide transition speed
-          pauseTime: 3000,                 // How long each slide will show
+          boxCols: 0,                     // For box animations
+          boxRows: 0,                     // For box animations
+          animSpeed: 1000,                 // Slide transition speed
+          pauseTime: 4000,                 // How long each slide will show
           startSlide: 0,                     // Set starting Slide (0 index)
           directionNav: true,             // Next & Prev navigation
           controlNav: false,                 // 1,2,3... navigation
